a6dcb93bd1938c6f1b2d59bee47aa7fbe8a7aab1
drac-0 <muhammadfaiz050607@gmail.com>
2026-04-27 07:55:34 +0700

    hard coded hash initial value

--- sha.c
+++ sha.c
@@ -10,6 +10,8 @@ typedef uint64_t u64t;
 typedef uint32_t u32t;
 
 //here is all the function.
+//wait which one is small sigma again?
+//fuck
 u32t ROTR(unsigned char num, int val){
       return  ((num >> val) | (num << (8-val)));
 }
@@ -95,6 +97,11 @@ void *Greg_bitLOC(char * Sinput, int len){
 //well
 //i guess that's my homework later.
 
+
+// as my promise, i will just hard coded the hash initial value, 
+// instead of calculating and allocating it by each call
+// hence, this part would be multi-lined comment
+/*
 u32t *hashVal1(){
       u32t * prime8 = malloc(8 * sizeof(u32t));
       u32t primes[8] = {2,3,5,7,11,13,17,19};
@@ -104,13 +111,19 @@ u32t *hashVal1(){
             double frac = modf(sqrt((double)primes[i]), &bb);
             u32t hashinitval= (u32t) (frac * 0x100000000ULL);
             prime8[i] = hashinitval;
+            if (i % 4 == 0){
+                  printf("\n");
+            }
+            printf("0x%08x, ",prime8[i] );
       }
+      printf("\n");
 
       return prime8;
 }
 
+
 u32t *hasval2(){
-      u32t *prime64 = malloc(64 * sizeof(u32t));
+      u32t *prime64 = malloc(65 * sizeof(u32t));
       u32t primes[64] = {
             2, 3, 5, 7, 
             11, 13, 17, 19,
@@ -136,14 +149,46 @@ u32t *hasval2(){
             double frac = modf(cbrt((double)primes[j]), &newbb);
             u32t other_Hashinitval = (u32t) (frac * 0x100000000ULL);
             prime64[j] = other_Hashinitval;
-            printf("H[%d] = 0x%08x\n", j, prime64[j] );
+            if (j % 4 == 0 ){
+                  printf("\n");
+            }
+            printf("0x%08x, ", prime64[j] );
       }
-
+      printf("\n");
       return prime64;
 }
-
+*/
 
 int main(){
+      u32t H[8] = {
+            0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 
+            0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 
+
+      };
+
+      u32t h[64] = {
+            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
+            0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 
+            0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 
+            0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 
+            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 
+            0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 
+            0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
+            0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 
+            0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 
+            0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 
+            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 
+            0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 
+            0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 
+            0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 
+            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 
+            0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+      };
+
+      // what to do next is to compute 64 word. 
+      // with W function that defined as :
+      // W(t) = 
+
       unsigned char word[10] = "abcd";
       unsigned char * gBITmain = Greg_bitLOC(word, strlen(word));
       free(gBITmain);
