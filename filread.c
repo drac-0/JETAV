4838b3bdafa33bb17dc4661259822eabbfebb868
drac-0 <muhammadfaiz050607@gmail.com>
2026-05-31 02:30:38 +0800

    check

--- filread.c
+++ filread.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "sha.c"
 
+typedef uint32_t u32t ;
 int main(){
-      FILE *f= fopen("data.txt","rb");
-      fseek(f, 0, SEEK_END);
-      long len = ftell(f);
-      rewind(f);
+      FILE *binREAD= fopen("test", "rb");
+      fseek(binREAD, 0, SEEK_END); //move cursor to the end 
+      long len = ftell(binREAD); //count the len of a file binary contect
+      rewind(binREAD); //what the fuck?
 
-      unsigned char * buffer = malloc(len);
-      fread(buffer,1,len,f);
-      fclose(f);
+      u32t * buffer = malloc(len); //where i will write the buffer
+      fread(buffer,1,len,binREAD); //read from f, 1-len, put it to buffer
 
-      u32t * H = sha256(buffer);
-      for (int p = 0; p < 8; p++){
-            printf("%08x", H[p]);
+      for (int i = 0 ; i < 8 ; i++){
+            printf("%08x", buffer[i]);
       }
-      printf("\n");
-      free(H);
       
+
 }
+
+
