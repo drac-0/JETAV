c7dabd7722b56ec6ebbf2493e2d1b9cb14876061
drac-0 <muhammadfaiz050607@gmail.com>
2026-06-30 20:10:52 +0800

    just a test

---
+++ CStringSUCKS/tstnpotd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "Chlara.h"
+
+int main(int argc, char *argv[])
+{
+      DDD dinar1 = {0,0,0};
+      append('a', &dinar1);
+      append('b', &dinar1);
+      append('c', &dinar1);
+      printAL2(&dinar1);
+
+
+}
