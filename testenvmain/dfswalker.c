68d36927bf2daa3cddde31cfbc5ed99fa2d4cdb7
drac-0 <muhammadfaiz050607@gmail.com>
2026-07-12 08:44:31 +0800

    complete dfs / walking program

--- testenvmain/dfswalker.c
+++ testenvmain/dfswalker.c
@@ -1,23 +1,47 @@
-#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <linux/limits.h>
 
-typedef struct stat stt ;
+void bjorn(char *path){
 
+      DIR *dir;
+      struct dirent *entry;
+      dir = opendir(path);
 
-int main(int argv, char *argc[]){
-      size_t count = 1;
-      while (argc[count]){
-            stt sb;
-            stat(argc[count], &sb);
-
-            
-
-            count++;
+      if (dir == NULL){
+            printf("can't open");
+            chdir("../");
+            return ;
       }
 
+      chdir(path);
+      while(entry = readdir(dir)){
+            if (entry->d_type == DT_DIR){
+                  if ((strcmp(entry->d_name, ".") == 0) 
+                        || (strcmp(entry->d_name, "..") == 0)){
+                        continue;
+                  }
+                  printf("folder name: %s\n", entry->d_name);
+                  bjorn(entry->d_name);
+            }
+            else if(entry->d_type == DT_REG){
+                  printf("file name: %s\n", entry->d_name);
+            }
+      }
+
+      chdir("../");
+
+      if (closedir(dir) == -1){
+            printf("Can't close");
+            return ;
+      }
 }
 
+int main(){
+      chdir("/home/draco/");
+      bjorn("./");
 
+      return 0 ;
+}
