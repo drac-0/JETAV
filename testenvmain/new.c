182cf3a6946abbe452c070ac2828fd7f0127bf76
drac-0 <muhammadfaiz050607@gmail.com>
2026-07-12 08:45:18 +0800

    untidy version

--- testenvmain/new.c
+++ testenvmain/new.c
@@ -1,21 +1,75 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <unistd.h>
+#include <linux/limits.h>
 
-void bjorn(){
+void bjorn(char *path){
 
+      DIR *dir;
+      struct dirent *entry;
+
+      dir = opendir(path);
+
+      if (dir == NULL){
+            printf("can't open");
+            chdir("../");
+            return ;
+      }
+      
+      chdir(path);
+
+      //debugging purpose
+      printf("current path: %s\n", path);
+      char cwd[PATH_MAX];
+      if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("Current working dir: %s\n", cwd);
+      }
+
+      else {
+            perror("getcwd() error");
+            return ;
+      }
+      while(entry = readdir(dir)){
+            if (entry->d_type == DT_DIR){
+                  if ((strcmp(entry->d_name, ".") == 0) 
+                        || (strcmp(entry->d_name, "..") == 0)){
+                        continue;
+                  }
+                  printf("recursion\n");
+                  bjorn(entry->d_name);
+            }
+            else if(entry->d_type == DT_REG){
+                  printf("file name: %s", entry->d_name);
+            }
+            printf("\n");
+      }
+
+      chdir("../");
+
+
+      if (closedir(dir) == -1){
+            printf("Can't close");
+            return ;
+      }
 }
 
 int main(){
+      chdir("/home/draco/");
+
+
+
+      /*
       DIR *dir ;
       struct dirent *entry;
 
-      dir = opendir("../");
+      dir = opendir("2pac/");
 
       if (dir == NULL){
             printf("Unable to open");
             return 1;
       }
+
       while (entry = readdir(dir)){
             if (entry->d_type == DT_DIR){
                   //filter
@@ -25,7 +79,6 @@ int main(){
                   }
                   printf("folder name: %s", entry->d_name);
             }
-
 
             else if(entry->d_type == DT_REG){
                   printf("file name: %s", entry->d_name);
@@ -37,6 +90,15 @@ int main(){
             printf("Can't close");
             return 1;
       }
+
+      printf("\n");
+      printf("\n");
+      printf("FUNC CALL SEPARATOR\n");
+      printf("\n");
+      printf("\n");
+      */
+
+      bjorn("./");
 
       return 0 ;
 }
