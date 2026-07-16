d6ae80138ec53510885c4a15dfffee1b191a0fa6
drac-0 <muhammadfaiz050607@gmail.com>
2026-07-16 08:50:44 +0800

    macros defined function

---
+++ assemblingTest1/headerUndUtil/sha.h
@@ -0,0 +1,43 @@
+#ifndef SHA
+#define SHA
+
+#include <stdint.h>
+typedef uint64_t u64t;
+typedef uint32_t u32t;
+
+#ifndef rotate
+#define rotate(num, val) ((num >> val) | (num << (32 - val)))
+#endif 
+
+#ifndef Sigma0
+#define Sigma0(x) (rotate(x, 7) ^ rotate(x,18) ^ (x >> 3)) 
+#endif 
+
+
+#ifndef Sigma1
+#define Sigma1(x) (rotate(x, 17) ^ rotate(x,19) ^ (x >> 10)) 
+#endif 
+
+
+#ifndef smallSig0
+#define smallSig0(x) (rotate(x, 2) ^ rotate(x,13) ^ rotate(x, 22)) 
+#endif 
+
+#ifndef smallSig1
+#define smallSig1(x) (rotate(x, 6) ^ rotate(x,11) ^ rotate(x, 25)) 
+#endif 
+
+#ifndef GCH
+#define GCH(x, y, z) ((x & y) ^ (~x & z))
+#endif // ifndef GCh
+
+#ifndef MAAAYOR
+#define MAAAYOR(x,y,z) ((x & y) ^ (x & z) ^ (y & z))
+#endif /* ifndef MAAAYOR(x,y,z) */
+
+
+void *Greg_bitLOC(unsigned char * Sinput, int len);
+u32t *wordCOME(unsigned char *gBIT);
+u32t * sha256(unsigned char * hasi);
+
+#endif // !SHA
